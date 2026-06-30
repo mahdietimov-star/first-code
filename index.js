c21f53b46a463507efd5df7913ff316cb2b2d721
Mahdi <Mahdi@DYUX1SETIMOV02.iom.int>
2026-06-30 19:56:23 +0500

    my first work

---
+++ index.js
@@ -0,0 +1,42 @@
+//var это глобная перемена
+//let это тоже переменая всеми использование
+//const это тоже переменая но не изменение 
+
+// var name= "Muhhamad";
+// console. log(name)
+
+// var age=20;
+// console как мы вызываем и где показать 
+
+// let a = 20;
+// console. log(a)
+// a = 30;
+// console. log(a)
+
+// let a1 = 45;
+// let b2 = 190;
+// console. log(a1+b2)
+
+// let a3 = 120;
+// let b4 = 4;
+// console. log(a3/b4)
+
+// let a5 = 100;
+// let b6 = 4;
+// console. log(a5 % b6)
+
+// function age (g, b){
+//     if (g <= 12 ){
+//         console.log("500 rubl")
+//     } else {
+//         console.log("1000 rubl")
+//     }if (b === "13student"){
+//         console.log("700 rubl")
+//     }else {
+//         console.log("1000 rubl")
+//     }
+
+// }
+// age(7, "ghghgh")
+// age(13, "13student")
+// age(23, "13student")
